f6c29a655dcedbe7ceca35d3ee5574445d8eed05
Andrey Smirnoff <149796739+AndreySmirnoffv@users.noreply.github.com>
2024-03-17 12:16:56 +0100

    Delete index directory

--- index/index.js
+++
@@ -1,9 +0,0 @@
-import {combineReducers, createStore} from "redux";
-import {basketReducer} from "./basketReducer/basketReducer";
-import { basketPrice } from "./basketReducer/basketPrice";
-
-const root = combineReducers({
-    basketReducer,
-    basketPrice
-})
-export const store = createStore(root);
